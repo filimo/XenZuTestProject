8c75693ad0a710f1d046d0e3893f864ee0419bc9
Viktor Kushnerov <filimo@gmail.com>
2021-06-22 07:48:39 +0300

    Init commit.

---
+++ XenZuTestProject/XenZuTestProjectApp.swift
@@ -0,0 +1,20 @@
+//
+//  XenZuTestProjectApp.swift
+//  XenZuTestProject
+//
+//  Created by Viktor Kushnerov on 22.06.21.
+//
+
+import SwiftUI
+
+@main
+struct XenZuTestProjectApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
